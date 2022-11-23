494e5aa6236aad48a0c07edd014b687cc137f449
teonactl <teonactl@hotmail.it>
2022-11-23 15:36:13 +0100

    Added RecycleView in Screen2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
+from kivy.uix.recycleview import RecycleView
 
 # The main kv file is loaded autoatically because of naming rules [ call it like your App class less the "App" eg: NavigationApp--> navigation.kv]
 Builder.load_file("screen1.kv")
@@ -10,10 +11,16 @@ Builder.load_file("screen3.kv")
 class MainScreen(MDScreen):
     pass
 
-class NavigationApp(MDApp):
+# Define the Recycleview class which is created in .kv file
+class RecycleViewer(RecycleView):
+    def __init__(self, **kwargs):
+        super(RecycleViewer, self).__init__(**kwargs)
+        self.data = [{'text': str(x)} for x in range(20)]
+
+class BandiApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
         return MainScreen()
 
 
-NavigationApp().run()
+BandiApp().run()
